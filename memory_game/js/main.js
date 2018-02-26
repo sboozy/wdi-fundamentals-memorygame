557c19beb499bfd5d325c90c676af15ee2de4b97
Stephanie B Burr <sbeusse@gmail.com>
2018-02-25 19:14:39 -0500

    added game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,17 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+  } else {
+  		alert("Sorry, try again.");
+	}
+}
